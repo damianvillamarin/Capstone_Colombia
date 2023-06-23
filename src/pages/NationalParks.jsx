8d232fb8a50d9003eecc4a38af0963028ba95f5d
MENDOZA <jairoe.mendoza@libertycolombia.com>
2023-06-23 11:14:18 -0500

    Minor improve

--- src/pages/NationalParks.jsx
+++ src/pages/NationalParks.jsx
@@ -1,13 +1,11 @@
 import React, { useState } from 'react';
-import {Container, Form} from 'react-bootstrap';
+import {Container, Form, Row, Col, Card} from 'react-bootstrap';
 import Select from "react-select";
 import data from "../data/nationalparks.json";
 import datastate from "../data/locations.json";
 import dataparktypes from "../data/parktypes.json";
-import Card from 'react-bootstrap/Card';
 
 const options = data.parks;
-
 
 const AppCard = (props) => {
     let currentOption = options.filter(e => e.State === props.children);
@@ -36,22 +34,51 @@ const AppCard = (props) => {
 
 const NationalParks = () => {
     const [selectedOption, setSelectedOption] = useState(0);
+    
     return (
         <Container>
-            <Form>
-                <Form.Group className="mb-3" controlId="control">
-                    <Form.Label>Select a Park:</Form.Label>
-                    <Select
-                        onChange={setSelectedOption}
-                        options={options}
-                        getOptionLabel={(option) => option.State}
-                        getOptionValue={(option) => option.State}
-                    />
-                </Form.Group>
-            </Form>
-            <AppCard>
-                {selectedOption.State}
-            </AppCard>
+            <Row className="text-center">
+                <h1>National Park Search</h1>
+            </Row>
+            <Row>
+                <Col>
+                    <h1>Radio Button</h1>
+                    <Form>
+                        <div key={`checkbox`} className="mb-3">
+                            <Form.Check 
+                                type={'checkbox'}
+                                label={`By Location`}
+                                id={`1`}
+                            />
+                            <Form.Check
+                                disabled
+                                type={'checkbox'}
+                                label={`By Park Type`}
+                                id={`2`}
+                            />
+                        </div>
+                    </Form>
+                </Col>
+                <Col>
+                    <h1>Select</h1>
+                    <Form>
+                        <Form.Group className="mb-3" controlId="control">
+                            <Form.Label>Select a Park:</Form.Label>
+                            <Select
+                                onChange={setSelectedOption}
+                                options={options}
+                                getOptionLabel={(option) => option.State}
+                                getOptionValue={(option) => option.State}
+                            />
+                        </Form.Group>
+                    </Form>
+                </Col>
+            </Row>
+            <Row>
+                <AppCard>
+                    {selectedOption.State}
+                </AppCard>
+            </Row>
         </Container>
     );
 }
