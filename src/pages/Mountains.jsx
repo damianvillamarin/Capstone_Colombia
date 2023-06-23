329cfc217b17035916752dbcd36cb8f1872761f9
MENDOZA <jairoe.mendoza@libertycolombia.com>
2023-06-23 02:22:58 -0500

    Fixed and improve Mountains js with React

--- src/pages/Mountains.jsx
+++ src/pages/Mountains.jsx
@@ -1,10 +1,54 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
+import React, { useState } from 'react';
+import {Container, Form} from 'react-bootstrap';
+import Select from "react-select";
+import data from "../data/mountains.json"
+import Card from 'react-bootstrap/Card';
+
+const options = data.mountains;
+
+const AppCard = (props) => {
+    let currentOption = options.filter(e => e.name === props.children);
+
+    if (currentOption.length > 0) {
+        return(
+            <Card style={{ width: '30rem' }} className="app-card">
+                <Card.Img variant="top" src={require(`../images/mountains/${currentOption[0].img}`)} />
+                <Card.Body>
+                    <Card.Title>
+                        {currentOption[0].name}
+                    </Card.Title>
+                    <Card.Text>
+                        {currentOption[0].desc}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    } else {
+        return(
+            <></>
+        );
+    }
+}
 
 const Mountains = () => {
+    const [selectedOption, setSelectedOption] = useState(0);
+    
     return (
         <Container>
-            Mountains
+            <Form>
+                <Form.Group className="mb-3" controlId="control">
+                    <Form.Label>Select a mountain:</Form.Label>
+                    <Select
+                        onChange={setSelectedOption}
+                        options={options}
+                        getOptionLabel={(option) => option.name}
+                        getOptionValue={(option) => option.name}
+                    />
+                </Form.Group>
+            </Form>
+            <AppCard>
+                {selectedOption.name}
+            </AppCard>
         </Container>
     );
 }
